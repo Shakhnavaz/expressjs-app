500e0bb1dd2ac623d51cbaa31408cbb711acb95c
Shakhnavaz <shakhnavaz@gmail.com>
2026-01-18 00:00:04 +0300

    переписан с нуля

--- app.js
+++ app.js
@@ -1,130 +1,121 @@
-// Получение пути к текущему файлу
-const getCurrentFilePath = () => {
-  return import.meta.url.substring(7);
-};
-
-const CORS_HEADERS = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-  "Access-Control-Allow-Headers": "*",
-};
-
-const TEXT_PLAIN_HEADER = {
-  "Content-Type": "text/plain; charset=utf-8",
-};
+import express from "express";
+import { createReadStream } from "fs";
+import crypto from "crypto";
+import http from "http";
+import bodyParser from "body-parser";
 
 const SYSTEM_LOGIN = "45cf3a7d-a058-4ede-a03c-2c98a130021d";
 
-/** Middleware для CORS */
-function corsMiddleware(req, res, next) {
-  res.set(CORS_HEADERS);
-  if (req.method === "OPTIONS") return res.sendStatus(204);
+const app = express();
+
+// Middleware для парсинга тела запроса
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// CORS headers
+app.use((req, res, next) => {
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+    "Access-Control-Allow-Headers": "*",
+  });
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
   next();
-}
+});
 
-/** Чтение файла через поток */
-function readFileAsync(filePath, createReadStream) {
-  return new Promise((resolve, reject) => {
-    const chunks = [];
-    const stream = createReadStream(filePath);
+// 5.1. Маршрут /login/
+app.get("/login/", (req, res) => {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send(SYSTEM_LOGIN);
+});
 
-    stream.on("data", (chunk) => chunks.push(chunk));
-    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
-    stream.on("error", (err) => reject(err));
+// 5.2. Маршрут /code/
+app.get("/code/", (req, res) => {
+  // Получение пути к текущему файлу
+  const filePath = import.meta.url.substring(7);
+  
+  const stream = createReadStream(filePath);
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  stream.pipe(res);
+  
+  stream.on("error", (err) => {
+    res.status(500).set("Content-Type", "text/plain; charset=utf-8").send(err.toString());
   });
-}
+});
 
-/** Генерация SHA1 хеша */
-function generateSha1Hash(text, crypto) {
-  return crypto.createHash("sha1").update(text).digest("hex");
-}
+// 5.3. Маршрут /sha1/:input/
+app.get("/sha1/:input/", (req, res) => {
+  const input = req.params.input;
+  const hash = crypto.createHash("sha1").update(input).digest("hex");
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send(hash);
+});
 
-/** Чтение данных из HTTP-ответа */
-function readHttpResponse(response) {
-  return new Promise((resolve, reject) => {
-    const chunks = [];
-    response.on("data", (chunk) => chunks.push(chunk));
-    response.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
-    response.on("error", (err) => reject(err));
+// 5.4. Маршрут /req/
+// GET /req/?addr=<url>
+app.get("/req/", (req, res) => {
+  const addr = req.query.addr;
+  
+  if (!addr) {
+    res.status(400).set("Content-Type", "text/plain; charset=utf-8").send("Parameter 'addr' is required");
+    return;
+  }
+  
+  http.get(addr, (response) => {
+    let data = "";
+    
+    response.on("data", (chunk) => {
+      data += chunk;
+    });
+    
+    response.on("end", () => {
+      res.set("Content-Type", "text/plain; charset=utf-8");
+      res.send(data);
+    });
+    
+    response.on("error", (err) => {
+      res.status(500).set("Content-Type", "text/plain; charset=utf-8").send(err.toString());
+    });
+  }).on("error", (err) => {
+    res.status(500).set("Content-Type", "text/plain; charset=utf-8").send(err.toString());
   });
-}
+});
 
-/** Универсальная функция для GET-запроса по URL */
-async function fetchUrlData(url, http) {
-  return new Promise((resolve, reject) => {
-    http.get(url, async (response) => {
-      try {
-        const data = await readHttpResponse(response);
-        resolve(data);
-      } catch (err) {
-        reject(err);
-      }
-    }).on("error", reject);
+// POST /req/ - addr из тела запроса
+app.post("/req/", (req, res) => {
+  const addr = req.body.addr;
+  
+  if (!addr) {
+    res.status(400).set("Content-Type", "text/plain; charset=utf-8").send("Parameter 'addr' is required");
+    return;
+  }
+  
+  http.get(addr, (response) => {
+    let data = "";
+    
+    response.on("data", (chunk) => {
+      data += chunk;
+    });
+    
+    response.on("end", () => {
+      res.set("Content-Type", "text/plain; charset=utf-8");
+      res.send(data);
+    });
+    
+    response.on("error", (err) => {
+      res.status(500).set("Content-Type", "text/plain; charset=utf-8").send(err.toString());
+    });
+  }).on("error", (err) => {
+    res.status(500).set("Content-Type", "text/plain; charset=utf-8").send(err.toString());
   });
-}
+});
 
-/** Создание Express-приложения */
-export function createApp(express, bodyParser, createReadStream, crypto, http) {
-  const app = express();
+// 5.5. Маршрут отлова остальных запросов
+app.all("*", (req, res) => {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send(SYSTEM_LOGIN);
+});
 
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
-  app.use(corsMiddleware);
-
-  // Возвращает системный логин
-  app.get("/login/", (_req, res) => {
-    res.set(TEXT_PLAIN_HEADER).send(SYSTEM_LOGIN);
-  });
-
-  // Возвращает содержимое текущего файла
-  app.get("/code/", async (_req, res) => {
-    const filePath = getCurrentFilePath();
-    const fileContent = await readFileAsync(filePath, createReadStream);
-    res.set(TEXT_PLAIN_HEADER).send(fileContent);
-  });
-
-  // Возвращает SHA1 хеш переданного параметра
-  app.get("/sha1/:input/", (req, res) => {
-    const hash = generateSha1Hash(req.params.input, crypto);
-    res.set(TEXT_PLAIN_HEADER).send(hash);
-  });
-
-  // GET /req/?addr=<url> - возвращает содержимое интернет-ресурса
-  app.get("/req/", async (req, res) => {
-    try {
-      const addr = req.query.addr;
-      if (!addr) {
-        res.status(400).set(TEXT_PLAIN_HEADER).send("Parameter 'addr' is required");
-        return;
-      }
-      const data = await fetchUrlData(addr, http);
-      res.set(TEXT_PLAIN_HEADER).send(data);
-    } catch (err) {
-      res.status(500).set(TEXT_PLAIN_HEADER).send(err.toString());
-    }
-  });
-
-  // POST /req/ - возвращает содержимое интернет-ресурса, addr из тела запроса
-  app.post("/req/", async (req, res) => {
-    try {
-      const addr = req.body.addr;
-      if (!addr) {
-        res.status(400).set(TEXT_PLAIN_HEADER).send("Parameter 'addr' is required");
-        return;
-      }
-      const data = await fetchUrlData(addr, http);
-      res.set(TEXT_PLAIN_HEADER).send(data);
-    } catch (err) {
-      res.status(500).set(TEXT_PLAIN_HEADER).send(err.toString());
-    }
-  });
-
-  // Любой другой маршрут возвращает системный логин
-  app.all(/.*/, (_req, res) => {
-    res.set(TEXT_PLAIN_HEADER).send(SYSTEM_LOGIN);
-  });
-
-  return app;
-}
-
-export { SYSTEM_LOGIN };
+export default app;
--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-import express from "express";
-import { createReadStream } from "fs";
-import crypto from "crypto";
-import http from "http";
-import bodyParser from "body-parser";
+import app from "./app.js";
 
-import { createApp } from "./app.js";
-
-const app = createApp(express, bodyParser, createReadStream, crypto, http);
-
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
