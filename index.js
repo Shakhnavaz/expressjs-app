f5131a023879af51f1ecb936792abbaf735c2337
Shakhnavaz <shakhnavaz@gmail.com>
2026-01-19 00:57:29 +0300

    add axios

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import bodyParser from "body-parser";
 import { createReadStream } from "fs";
 import crypto from "crypto";
 import http from "http";
+import axios from 'axios';
 import appSrc from "./app.js";
 
-const app = appSrc(express, bodyParser, createReadStream, crypto, http);
+const app = appSrc(express, bodyParser, createReadStream, crypto, http, axios);
 
 app.listen(process.env.PORT || 3000);
