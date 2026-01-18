07fd913f283a371a9580bb303e722349912b843e
Shakhnavaz <shakhnavaz@gmail.com>
2026-01-18 17:41:52 +0300

    puppeteer 1

--- app.js
+++ app.js
@@ -1,66 +1,143 @@
-import express from "express";
-import puppeteer from "puppeteer";
-
-const app = express();
-
-/* CORS */
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  next();
-});
-
-/* /login/ */
-app.get("/login/", (_, res) => {
-  res.type("text/plain");
-  res.send("45cf3a7d-a058-4ede-a03c-2c98a130021d");
-});
-
-/* /test/?URL=... */
-app.get("/test/", async (req, res) => {
-  const targetURL = req.query.URL;
-
-  if (!targetURL) {
-    res.status(400).type("text/plain").send("URL query parameter is required");
-    return;
-  }
-
-  let browser;
-
-  try {
-    browser = await puppeteer.launch({
-      headless: "new",
-      executablePath: "/usr/bin/chromium-browser",
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+export default function(express, bodyParser, createReadStream, crypto, http) {
+  const SYSTEM_LOGIN = "45cf3a7d-a058-4ede-a03c-2c98a130021d";
+  
+  const app = express();
+  
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  
+  app.use((req, res, next) => {
+    res.set({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,OPTIONS,DELETE",
+      "Access-Control-Allow-Headers": "*",
     });
-
-    const page = await browser.newPage();
-    await page.goto(targetURL, { waitUntil: "networkidle2" });
-
-    await page.waitForSelector("#bt", { timeout: 3000 });
-    await page.waitForSelector("#inp", { timeout: 3000 });
-
-    await page.click("#bt");
-
-    await page.waitForFunction(() => {
-      const input = document.querySelector("#inp");
-      return input && input.value !== "";
-    }, { timeout: 3000 });
-
-    const result = await page.evaluate(() => {
-      return document.querySelector("#inp").value;
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(204);
+    }
+    next();
+  });
+  
+  app.get("/login/", (req, res) => {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send(SYSTEM_LOGIN);
+  });
+  
+  app.get("/code/", (req, res) => {
+    const filePath = import.meta.url.substring(7);
+    const stream = createReadStream(filePath);
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    stream.pipe(res);
+    stream.on("error", (err) => {
+      res.status(500).set("Content-Type", "text/plain; charset=utf-8").send(err.toString());
     });
+  });
+  
+  app.get("/sha1/:input/", (req, res) => {
+    const input = req.params.input;
+    const hash = crypto.createHash("sha1").update(input).digest("hex");
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send(hash);
+  });
+  
+  app.get("/req/", (req, res) => {
+    const addr = req.query.addr;
+    if (!addr) {
+      res.status(400).set("Content-Type", "text/plain; charset=utf-8").send("Parameter 'addr' is required");
+      return;
+    }
+    http.get(addr, (response) => {
+      let data = "";
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+      response.on("end", () => {
+        res.set("Content-Type", "text/plain; charset=utf-8");
+        res.send(data);
+      });
+      response.on("error", (err) => {
+        res.status(500).set("Content-Type", "text/plain; charset=utf-8").send(err.toString());
+      });
+    }).on("error", (err) => {
+      res.status(500).set("Content-Type", "text/plain; charset=utf-8").send(err.toString());
+    });
+  });
+  
+  app.post("/req/", (req, res) => {
+    const addr = req.body.addr;
+    if (!addr) {
+      res.status(400).set("Content-Type", "text/plain; charset=utf-8").send("Parameter 'addr' is required");
+      return;
+    }
+    http.get(addr, (response) => {
+      let data = "";
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+      response.on("end", () => {
+        res.set("Content-Type", "text/plain; charset=utf-8");
+        res.send(data);
+      });
+      response.on("error", (err) => {
+        res.status(500).set("Content-Type", "text/plain; charset=utf-8").send(err.toString());
+      });
+    }).on("error", (err) => {
+      res.status(500).set("Content-Type", "text/plain; charset=utf-8").send(err.toString());
+    });
+  });
+  
+  app.get("/test/", async (req, res) => {
+    const pageUrl = req.query.URL;
 
-    res.type("text/plain").send(result);
+    if (!pageUrl) {
+      res
+        .status(400)
+        .set("Content-Type", "text/plain; charset=utf-8")
+        .send("Parameter 'URL' is required");
+      return;
+    }
 
-  } catch (e) {
-    res.status(500).type("text/plain").send("Error processing page");
-  } finally {
-    if (browser) await browser.close();
-  }
-});
+    try {
+      const puppeteer = await import("puppeteer");
 
-/* HTTP */
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+      const browser = await puppeteer.default.launch({
+        headless: "new",
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      });
+
+      const page = await browser.newPage();
+      await page.goto(pageUrl, { waitUntil: "networkidle2" });
+
+      // Ждём кнопку и кликаем
+      await page.waitForSelector("#bt", { timeout: 5000 });
+      await page.click("#bt");
+
+      // Ждём появления значения в поле ввода
+      await page.waitForFunction(() => {
+        const inp = document.querySelector("#inp");
+        return inp && inp.value !== "";
+      }, { timeout: 5000 });
+
+      const value = await page.$eval("#inp", el => el.value);
+
+      await browser.close();
+
+      res.set("Content-Type", "text/plain; charset=utf-8");
+      res.send(value.toString());
+
+    } catch (err) {
+      res
+        .status(500)
+        .set("Content-Type", "text/plain; charset=utf-8")
+        .send(err.toString());
+    }
+  });
+
+
+  app.all("*", (req, res) => {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send(SYSTEM_LOGIN);
+  });
+  
+  return app;
+}
